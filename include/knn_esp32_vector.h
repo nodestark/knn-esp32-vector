29947c9237a9924705097155643641d75f09c4cd
Nodestark <104593465+nodestark@users.noreply.github.com>
2025-05-18 18:01:56 -0300

    Create knn_esp32_vector.h
    Initial import

---
+++ include/knn_esp32_vector.h
@@ -0,0 +1,14 @@
+#ifndef KNN_ESP32_VECTOR_H
+#define KNN_ESP32_VECTOR_H
+
+typedef struct {
+	int id;
+	float data[VECT_DIM];
+	float distance;
+	void *next;
+} vector_entry_t;
+
+void add_vector(int id, float *vct);
+vector_entry_t* query_nn(float *vct);
+
+#endif
