42b9a9da0b334d9b6dd728eddb6e4966575d768a
leonardo soares de souza <leonardobsi@gmail.com>
2025-05-18 18:19:25 -0300

    refacting

--- include/knn_esp32_vector.h
+++ include/knn_esp32_vector.h
@@ -1,11 +1,13 @@
 #ifndef KNN_ESP32_VECTOR_H
 #define KNN_ESP32_VECTOR_H
 
+#define VECT_DIM 	4
+
 typedef struct {
 	int id;
-	float data[VECT_DIM];
-	float distance;
-	void *next;
+    float data[VECT_DIM];
+    float distance;
+    void *next;
 } vector_entry_t;
 
 void add_vector(int id, float *vct);
--- knn_esp32_vector.c
+++ knn_esp32_vector.c
@@ -5,14 +5,7 @@
 #include <math.h>
 #include <string.h>
 
-#define VECT_DIM 	4
-
-typedef struct {
-	int id;
-    float data[VECT_DIM];
-    float distance;
-    void *next;
-} vector_entry_t;
+#include "knn_esp32_vector.h"
 
 vector_entry_t *vector_entry_db = (void*) 0;
 
@@ -61,28 +54,4 @@ vector_entry_t* query_nn(float *vct){
 	}
 
 	return vector_entry_;
-}
-
-int main() {
-
-	float db[5][4] = {
-	        {1.1, 2.2, 3.3, 4.4},
-	        {5.5, 6.6, 7.7, 8.8},
-	        {9.9, 10.1, 11.1, 12.2},
-	        {13.3, 14.4, 15.5, 16.6},
-	        {17.7, 18.8, 19.9, 20.0}};
-
-	add_vector(0, db[0]);
-	add_vector(1, db[1]);
-	add_vector(2, db[2]);
-	add_vector(3, db[3]);
-	add_vector(4, db[4]);
-
-	float query[] = {8.9, 40.6, 1.1, 2.2};
-
-	vector_entry_t *vector_entry = query_nn((float*) &query);
-
-	printf(">_ %d\n", vector_entry->id);
-
-	return 0;
 }
